9b53f531eb2164be24227caaf5579ae7e84c3ff8
manel turki <menelturki@gmail.com>
2020-03-18 09:31:11 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+         {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
       </div>
     )
   }
